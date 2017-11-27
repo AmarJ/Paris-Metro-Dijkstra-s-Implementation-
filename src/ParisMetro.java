0e03763b425746706c5e65f18c2fc3dd03a96f92
Amar Jasarbasic <amarjasarbasic@gmail.com>
2017-11-27 00:06:21 -0500

    added opening file in readMetro

--- src/ParisMetro.java
+++ src/ParisMetro.java
@@ -1,13 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ParisMetro {
-	
-	public ParisMetro(){
-		//empty for now
+
+    private int verticesSize;
+    private int edgesSize;
+
+	public ParisMetro(int vSize, int eSize){
+		this.verticesSize = vSize;
+		this.edgesSize = eSize;
 	}
 	
-	public static void readMetro(file fileName){
-
+	public static void readMetro(String fileName){
+        try {
+            FileReader file = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open file.");
+        }
 
 	}
+
+    public static void main(String args[]){
+
+	    readMetro("../data/rawMetroData.txt");
+
+    }
 	
 	
 }
