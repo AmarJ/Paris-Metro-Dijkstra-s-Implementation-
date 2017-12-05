7dd85fc4b293739947671d1a6d5d015a0ff29bba
Amar Jasarbasic <amarjasarbasic@gmail.com>
2017-12-05 13:43:08 -0500

    added lines

--- src/ParisMetro.java
+++ src/ParisMetro.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class ParisMetro {
 
-    List<Station> nodes;
-    List<Route> edges;
-    Set<Station> settled;
-    Set<Station> unSettled;
-    Map<Station, Station> predecessors;
-    Map<Station, Integer> distance;
+    private List<Station> nodes;
+    private List<Route> edges;
+    private Set<Station> settled;
+    private Set<Station> unSettled;
+    private Map<Station, Station> predecessors;
+    private Map<Station, Integer> distance;
+    private static final int WALKING_TIME_CONST = 90;
 
     public ParisMetro(){
         Graph graph = new Graph("../data/metro.txt");
@@ -16,7 +17,40 @@ public class ParisMetro {
         this.edges = graph.getRoutes();
     }
 
+    public LinkedList<Integer> findMostEfficientPath(int sourceNumber){
+
+        //Implementation of BFS to find line corresponding to station source
+
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        LinkedList<Integer> path = new LinkedList<Integer>();
+
+        boolean visited[] = new boolean[nodes.size()];
+
+        visited[sourceNumber] = true;
+        queue.add(sourceNumber);
+
+        while (queue.size() != 0){
+            int currentSourceNumber = queue.poll();
+            Station currentSource = nodes.get(currentSourceNumber);
+            path.add(currentSourceNumber);
+
+            for (Route current : this.edges){
+                if (current.getSource().equals(currentSource) && current.getWeight() != -1){
+                    if (!visited[current.getDestination().getStationNumber()]){
+                        visited[current.getDestination().getStationNumber()] = true;
+                        queue.add(current.getDestination().getStationNumber());
+                    }
+                }
+            }
+        }
+
+        return path;
+    }
+
     public LinkedList<Integer> findMostEfficientPath(int sourceNumber, int destinationNumber){
+
+        //Most efficient path between source and destination nodes in metro graph
+
         Station source = nodes.get(sourceNumber);
         Station destination = nodes.get(destinationNumber);
 
@@ -50,6 +84,26 @@ public class ParisMetro {
 
     public LinkedList<Integer> findMostEfficientPath(int sourceNumber, int destinationNumber, int omitStation){
 
+        LinkedList<Integer> stationsToRemove = findMostEfficientPath(omitStation);
+        List<Station> nodesToRemove = new ArrayList<Station>();
+        List<Route> routeToRemove = new ArrayList<Route>();
+
+        for (Integer toRemove : stationsToRemove){
+            for (Route current : this.edges){
+                if (current.getSource().equals(nodes.get(toRemove)) && current.getWeight() != -1){
+                    routeToRemove.add(current);
+                    nodesToRemove.add(nodes.get(toRemove));
+                }
+            }
+        }
+
+        for (Station x : nodesToRemove){
+            System.out.print(x.getStationNumber()+" ");
+        }
+
+        nodes.removeAll(nodesToRemove);
+        System.out.println(nodes.contains(nodes.get(2)));
+        edges.removeAll(routeToRemove);
 
         Station source = nodes.get(sourceNumber);
         Station destination = nodes.get(destinationNumber);
@@ -113,6 +167,9 @@ public class ParisMetro {
     private int helperGetDistance(Station node, Station target){
         for (Route current : edges){
             if (current.getSource().equals(node) && current.getDestination().equals(target)) {
+                if (current.getWeight() == -1){
+                    return WALKING_TIME_CONST;
+                }
                 return current.getWeight();
             }
         }
@@ -147,11 +204,14 @@ public class ParisMetro {
     }
 
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         ParisMetro test = new ParisMetro();
 
-        if (args.length == 2) {
+        if (args.length == 1){
+            LinkedList<Integer> path = test.findMostEfficientPath(Integer.parseInt(args[0]));
+            System.out.println(path.toString());
+        } else if (args.length == 2) {
             LinkedList<Integer> path = test.findMostEfficientPath(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
             System.out.println(path.toString());
             System.out.println(test.getMostEfficientTravelTime(path));
